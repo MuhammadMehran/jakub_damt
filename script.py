497f82f31b92d093f00efb54a0b870c87ed86dc9
MuhammadMehran <mehranbutt97@gmail.com>
2020-03-27 21:47:39 +0500

    login add

--- script.py
+++ script.py
@@ -7,11 +7,24 @@ from selenium.webdriver.common.keys import Keys
 
 path = '/Users/EC9WU4/Downloads/Python/Book2.xlsx'
 
+website = ''
+username = ''
+password = ''
+
+
 driver = webdriver.Chrome()
 
 
 
 driver = webdriver.Chrome(executable_path='/Users/EC9WU4/Downloads/chromedriver 3')
+
+driver.get(website)
+
+driver.find_element_by_xpath('//input[@type="text"]').send_keys(username)
+
+driver.find_element_by_xpath('//input[@type="password"]').send_keys(password)
+driver.find_element_by_xpath('//input[@type="password"]').send_keys(Keys.RETURN)
+
 df = pd.read_excel(path)
 
 for index, row in df.iterrows():
